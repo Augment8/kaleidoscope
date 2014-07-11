23365125cecdf86f313d0d01780f2791a0540629
Tomohiko Himura <eiel.hal@gmail.com>
2014-07-11 11:43:12 +0900

    セカンドスクリーン対応

--- SepView/ViewController.swift
+++ SepView/ViewController.swift
@@ -47,6 +47,7 @@ class BoxViewCreator {
 }
 
 class ViewController: UIViewController {
+    let mainView: UIView = UIView()
     var imageViews: [UIImageView] = []
     let motionManager: CMMotionManager = CMMotionManager()
     let gravity: UIGravityBehavior = UIGravityBehavior()
@@ -78,6 +79,8 @@ class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpScreenConnectionNotificationHandlers()
+        checkForExistingScreenAndInitilaizePresent()
         srand(CUnsignedInt(time(nil)))
         self.view.backgroundColor = UIColor.blackColor()
         // Do any additional setup after loading the view, typically from a nib.
@@ -114,16 +117,6 @@ class ViewController: UIViewController {
         self.view.addSubview(creator.view)
         creator2.view.frame = CGRectMake(100, 0, 100, 100)
         self.view.addSubview(creator2.view)
-
-        let size = 24
-        let imageRect = CGRect(x: 140,y: 100,width: 40,height: 100);
-        for i in 0..<size {
-            let imageView = UIImageView()
-            imageView.frame = imageRect
-            self.view.addSubview(imageView)
-            imageViews.append(imageView)
-        }
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -133,16 +126,58 @@ class ViewController: UIViewController {
     
     func loop(link: CADisplayLink) {
         let image = ViewController.capturedImageWithView([creator.view,creator2.view])
-        var n: CGFloat = 0
-        let transform = CGAffineTransformMakeTranslation(0, 100);
         for imageView in imageViews {
             imageView.image = image
-            let angle:CGFloat = n * CGFloat(M_PI) / 180.0;
-            imageView.transform = CGAffineTransformTranslate(CGAffineTransformRotate(transform, angle), 0,-100)
-            n += 360 / CGFloat(imageViews.count)
         }
     }
 
+    func setupWindow(newScreen: UIScreen) {
+        let screenBounds: CGRect = newScreen.bounds
+        secondWindow = UIWindow(frame: screenBounds)
+        secondWindow!.screen = newScreen
+        secondWindow!.hidden = false
+        mainView.frame = newScreen.bounds
+        secondWindow!.addSubview(mainView)
 
+        let size = 18
+        let width = newScreen.bounds.width > newScreen.bounds.height ? newScreen.bounds.height : newScreen.bounds.width
+        let imageRect = CGRect(x: newScreen.bounds.width / 2,y: newScreen.bounds.height / 8,width: 80,height: width/4)
+        var n: CGFloat = 0
+        for i in 0..<size {
+            let imageView = UIImageView()
+            imageView.frame = imageRect
+            mainView.addSubview(imageView)
+            imageViews.append(imageView)
+
+            let height = imageView.frame.height
+            let transform = CGAffineTransformMakeTranslation(0, width/4);
+            let angle:CGFloat = n * CGFloat(M_PI) / 180.0;
+            imageView.transform = CGAffineTransformTranslate(CGAffineTransformRotate(transform, angle), 0,-width/4)
+            n += 360 / CGFloat(size)
+        }
+    }
+
+    func checkForExistingScreenAndInitilaizePresent() {
+        if UIScreen.screens().count > 1 {
+            let newScreen: UIScreen = UIScreen.screens()[1] as UIScreen
+            setupWindow(newScreen)
+        }
+    }
+
+    func setUpScreenConnectionNotificationHandlers() {
+        let center = NSNotificationCenter.defaultCenter()
+        center.addObserver(self, selector: "handleScreenDidConnectNotification:", name: UIScreenDidConnectNotification, object: nil)
+        center.addObserver(self, selector: "handleScreenDidDisconnectNotification:", name: UIScreenDidDisconnectNotification, object: nil)
+    }
+
+    func handleScreenDidConnectNotification(notification: NSNotification) {
+        let newScreen: UIScreen = notification.object as UIScreen
+        if (!secondWindow) {
+            setupWindow(newScreen)
+        }
+    }
+
+    func handleScreenDidDisconnectNotification(notification: NSNotification) {
+    }
 }
 
